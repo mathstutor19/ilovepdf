b1c9dfe34b2460c290c735d3ac844d7b80aaa96c
Bekmirzayev <mathstutor19@gmail.com>
2022-07-05 21:11:59 +0500

    mege created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Merge from "./pages/Merge/Merge";
 
 function App() {
-    return <div className="App">home</div>;
+    return (
+        <div className="App">
+            <Merge />
+        </div>
+    );
 }
 
 export default App;
