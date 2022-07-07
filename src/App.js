5d66276d7cd2e2187147cdfbb053e649806b61b9
akramboy05 <akramboy05r@gmail.com>
2022-07-07 20:27:26 +0500

    register-login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import "./App.css";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Context } from "./context/Context";
+
+
+// import components
+import Navbar from './components/Navbar/Navbar';
 import Footer from "./components/footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./routes/Home/Home";
+
+
+// import pages
+import Home from './routes/Home/Home';
+import Login from './routes/Login/Login';
+import Register from './routes/Register/Register';
+
 
 function App() {
-    return <div className="App">
-        <Router>
-            <Navbar/>
-            <Switch>
-            <Route exact path="/" component={Home}/>
-            </Switch>
-            <Footer/>
-        </Router>
-    </div>;
+  return (
+    <Context.Provider value={''} >
+      <BrowserRouter>
+
+        <Navbar/>
+
+        <Routes>
+
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+
+        </Routes>
+    
+        <Footer/>
+        
+      </BrowserRouter>
+    </Context.Provider>
+  );
 }
 
 export default App;
