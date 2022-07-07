69e5ed6edb67b52cc7b423c08c26c16e87aec51f
akramboy05 <akramboy05r@gmail.com>
2022-07-07 19:58:28 +0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./routes/Home/Home";
 
 function App() {
     return <div className="App">
-    <Home/>
+        <Router>
+            <Navbar/>
+            <Switch>
+            <Route exact path="/" component={Home}/>
+            </Switch>
+            <Footer/>
+        </Router>
     </div>;
 }
 
--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Banner from '../../components/Banner/Banner'
-import Footer from '../../components/footer/Footer'
-import Navbar from '../../components/Navbar/Navbar'
+
 import "./Home.css"
 const Home = () => {
   return (
     <div className='home'>
-        <Navbar/>
         <Banner/>
-        <Footer/>
     </div>
   )
 }
